4086c93efcc0d4834a06cdc792d101a56a5917c9
Yaman Umuroglu <maltanar@gmail.com>
2015-12-14 12:48:44 +0100

    bitfile loader script for ZedBoard Linux

---
+++ platform-wrapper/zedboard-linux/load-bitfile.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# See here for an example of using Vivado to convert regular bitfiles to
+# the appropriate binfile format for the ZedBoard:
+# https://github.com/maltanar/spmv-vector-cache/tree/master/bitfiles
+
+BITFILE_PATH="/root/bitfiles/$1.bin"
+
+if [ ! -f /tmp/foo.txt ]; then
+  echo "File not found!"
+  exit 1
+else
+  echo "Loading bitfile: $BITFILE_PATH"
+  cat $BITFILE_PATH > /dev/xdevcfg
+fi
+
+
+
