805b42b24a64b4b865f3fdcad7220eafb3da4531
Yaman Umuroglu <maltanar@gmail.com>
2016-01-12 09:23:06 +0100

    assume accel mem pool for linux is located at 0x10000000
    use the mem= parameter on the kernel command line to limit as
    appropriate

--- platform-wrapper/regdriver/platform-zedboard-linux.cpp
+++ platform-wrapper/regdriver/platform-zedboard-linux.cpp
@@ -1,5 +1,15 @@
 // platform init-deinit functions for Linux on the ZedBoard
-// note that this assumes the peripheral lives at address 0x43c00000
+// note that this assumes the peripheral lives at address 0x43c00000, and that 256MB of
+// unmanaged memory is available at address 0x10000000
+
+/*
+something like this can be used for the ZedBoard to ensure the kernel leaves the upper half
+of the DDR alone:
+
+env set fdt_high 0x10000000
+env set initrd_high 0x10000000
+env set bootargs "console=ttyPS0,115200 root=/dev/mmcblk0p2 rw rootwait earlyprintk cma=16m mem=256m"
+*/
 
 #include "platform.h"
 #include "linuxphysregdriver.hpp"
@@ -17,7 +27,7 @@ LinuxPhysRegDriver * platform = 0;
 
 WrapperRegDriver * initPlatform() {
   if(!platform) {
-    platform = new LinuxPhysRegDriver((void *) 0x43c00000, (void *) 0x17000000, 128 * 1024 * 1024);
+    platform = new LinuxPhysRegDriver((void *) 0x43c00000, (void *) 0x10000000, 256 * 1024 * 1024);
   }
   return (WrapperRegDriver *) platform;
 }
