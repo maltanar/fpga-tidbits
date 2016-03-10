19a7220978bd8954757e8801d7441b5fb68f35bd
Yaman Umuroglu <maltanar@gmail.com>
2016-03-10 23:19:40 +0000

    support Chisel printfs in verilator flow
    needs recent version of verilator to work

--- src/main/script/verilator-build.sh
+++ src/main/script/verilator-build.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-VERILATOR_SRC_DIR="/usr/share/verilator/include"
+# requires a recent version of verilator, e.g. 3.878
+VERILATOR_SRC_DIR="/usr/local/share/verilator/include"
 
 # call verilator to translate verilog to C++
-verilator --cc TesterWrapper.v +define+SYNTHESIS=1 -Wno-fatal -Wno-lint -Wno-style -Wno-COMBDLY --Mdir verilated
+verilator --cc TesterWrapper.v -Wno-assignin -Wno-fatal -Wno-lint -Wno-style -Wno-COMBDLY -Wno-STMTDLY --Mdir verilated
+# if verilator freezes while executing, consider adding +define+SYNTHESIS=1
+# to the cmdline here. this will disable the Chisel printfs though.
+
 # add verilated.cpp from source dirs
 cp -f $VERILATOR_SRC_DIR/verilated.cpp .
 # compile everything
 g++ -std=c++11 -I$VERILATOR_SRC_DIR -Iverilated *.cpp verilated/*.cpp -o VerilatedTesterWrapper
-
