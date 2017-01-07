6e611b80b735953c4b4ee332ca99987dad4a2738
Yaman Umuroglu <maltanar@gmail.com>
2017-01-07 01:02:35 +0100

    plug unused valid signals in TestCopy

--- src/main/scala/fpgatidbits/platform-wrapper/tests/TestCopy.scala
+++ src/main/scala/fpgatidbits/platform-wrapper/tests/TestCopy.scala
@@ -22,35 +22,43 @@ class TestCopy(p: PlatformWrapperParams) extends GenericAccelerator(p) {
   val rrg = Module(new ReadReqGen(p.toMemReqParams(), 0, 1)).io
   val wrg = Module(new WriteReqGen(p.toMemReqParams(), 0, 8)).io
 
+  val readPort = io.memPort(0)
+  val writePort = io.memPort(1)
+
+  // plug unused valid signals
+  readPort.memWrReq.valid := Bool(false)
+  readPort.memWrDat.valid := Bool(false)
+  writePort.memRdReq.valid := Bool(false)
+
   rrg.ctrl.start := io.start
   rrg.ctrl.throttle := Bool(false)
   rrg.ctrl.baseAddr := io.srcAddr
   rrg.ctrl.byteCount := io.byteCount
-  rrg.reqs <> io.memPort(0).memRdReq
+  rrg.reqs <> readPort.memRdReq
 
   wrg.ctrl.start := io.start
   wrg.ctrl.throttle := Bool(false)
   wrg.ctrl.baseAddr := io.dstAddr
   wrg.ctrl.byteCount := io.byteCount
-  wrg.reqs <> io.memPort(1).memWrReq
+  wrg.reqs <> writePort.memWrReq
 
   // pull out read response as write data
-  ReadRespFilter(io.memPort(0).memRdRsp) <> io.memPort(1).memWrDat
+  ReadRespFilter(readPort.memRdRsp) <> writePort.memWrDat
 
   // count write responses to determine finished
   val regNumPendingReqs = Reg(init = UInt(0, 32))
   val regRequestedBytes = Reg(init = UInt(0, 32))
 
-  io.memPort(1).memWrRsp.ready := Bool(true)
+  writePort.memWrRsp.ready := Bool(true)
 
   when(!io.start) {
     regNumPendingReqs := UInt(0)
     regRequestedBytes := UInt(0)
   } .otherwise {
-    val reqFired = io.memPort(1).memWrReq.valid & io.memPort(1).memWrReq.ready
-    val rspFired = io.memPort(1).memWrRsp.valid & io.memPort(1).memWrRsp.ready
+    val reqFired = writePort.memWrReq.valid & writePort.memWrReq.ready
+    val rspFired = writePort.memWrRsp.valid & writePort.memWrRsp.ready
 
-    regRequestedBytes := regRequestedBytes + Mux(reqFired, io.memPort(1).memWrReq.bits.numBytes, UInt(0))
+    regRequestedBytes := regRequestedBytes + Mux(reqFired, writePort.memWrReq.bits.numBytes, UInt(0))
 
     when(reqFired && !rspFired) { regNumPendingReqs := regNumPendingReqs + UInt(1)}
     .elsewhen(!reqFired && rspFired) { regNumPendingReqs := regNumPendingReqs - UInt(1) }
