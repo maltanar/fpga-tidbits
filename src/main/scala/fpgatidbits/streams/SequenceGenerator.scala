287664d935d6238f9df2ef5f77414f25b9ac471d
Yaman Umuroglu <maltanar@gmail.com>
2018-02-06 22:10:23 +0100

    allow using fewer addr bits for SequenceGenerator

--- src/main/scala/fpgatidbits/streams/SequenceGenerator.scala
+++ src/main/scala/fpgatidbits/streams/SequenceGenerator.scala
@@ -5,18 +5,18 @@ import Chisel._
 // Generates an arithmetic sequence with a given <init> and <step>
 // with <count> elements. Example: <init>=1 <step>=2 <count>=4
 // seq = 1 3 5 7
-class SequenceGenerator(w: Int) extends Module {
+class SequenceGenerator(w: Int, a: Int = 32) extends Module {
   val io = new Bundle {
     val start = Bool(INPUT)
     val init = UInt(INPUT, width = w)
-    val count = UInt(INPUT, width = 32)
+    val count = UInt(INPUT, width = a)
     val step = UInt(INPUT, width = w)
     val finished = Bool(OUTPUT)
     val seq = Decoupled(UInt(width = w))
   }
 
   val regSeqElem = Reg(init = UInt(0, w))
-  val regElemsLeft = Reg(init = UInt(0, 32))
+  val regElemsLeft = Reg(init = UInt(0, a))
   io.finished := Bool(false)
   io.seq.valid := Bool(false)
   io.seq.bits := regSeqElem
