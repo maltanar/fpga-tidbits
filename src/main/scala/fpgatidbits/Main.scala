66dc4e22cb59fa306e6e0f0c17a471a40f96341d
Yaman Umuroglu <maltanar@gmail.com>
2018-06-19 23:16:05 +0100

    change PYNQ platform name to PYNQZ1

--- src/main/scala/fpgatidbits/Main.scala
+++ src/main/scala/fpgatidbits/Main.scala
@@ -18,7 +18,7 @@ object TidbitsMakeUtils {
 
   val platformMap: PlatformMap = Map(
     "ZedBoard" -> {f => new ZedBoardWrapper(f)},
-    "PYNQ" -> {f => new PYNQWrapper(f)},
+    "PYNQZ1" -> {f => new PYNQWrapper(f)},
     "ZC706" -> {f => new ZC706Wrapper(f)},
     "WX690T" -> {f => new WolverinePlatformWrapper(f)},
     "Tester" -> {f => new TesterWrapper(f)}
