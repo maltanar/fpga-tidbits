3a28a6856ce8011850d5054a1b453c76171ff225
Yaman Umuroglu <maltanar@gmail.com>
2018-10-17 21:58:06 +0100

    start adding PYNQU96Wrapper for PYNQ on Ultra-96

---
+++ src/main/scala/fpgatidbits/platform-wrapper/axi/PYNQU96Wrapper.scala
@@ -0,0 +1,26 @@
+package fpgatidbits.PlatformWrapper
+
+import Chisel._
+
+// platform wrapper for PYNQ on Ultra-96
+
+object PYNQU96Params extends PlatformWrapperParams {
+  val platformName = "PYNQU96"
+  val memAddrBits = 32
+  val memDataBits = 64
+  val memIDBits = 6
+  val memMetaBits = 1
+  val numMemPorts = 1
+  val sameIDInOrder = true
+  val typicalMemLatencyCycles = 32
+  val burstBeats = 8  // TODO why cap bursts at 8? AXI can do more
+}
+
+class PYNQU96Wrapper(instFxn: PlatformWrapperParams => GenericAccelerator)
+  extends AXIPlatformWrapper(PYNQU96Params, instFxn) {
+  val platformDriverFiles = baseDriverFiles ++ Array[String](
+    "platform-xlnk.cpp", "xlnkdriver.hpp"
+  )
+  setName("PYNQU96Wrapper")
+  setModuleName("PYNQU96Wrapper")
+}
