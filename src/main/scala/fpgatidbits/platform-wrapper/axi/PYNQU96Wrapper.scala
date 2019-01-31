8ebd836409727bf4b83ff625da48909d1d3a9008
Davide Conficconi <davideconficconi@gmail.com>
2019-01-31 11:34:36 +0000

    update ultra96 wrapper with 4 memory ports
    Signed-off-by: Davide Conficconi <davideconficconi@gmail.com>

--- src/main/scala/fpgatidbits/platform-wrapper/axi/PYNQU96Wrapper.scala
+++ src/main/scala/fpgatidbits/platform-wrapper/axi/PYNQU96Wrapper.scala
@@ -10,7 +10,7 @@ object PYNQU96Params extends PlatformWrapperParams {
   val memDataBits = 64
   val memIDBits = 6
   val memMetaBits = 1
-  val numMemPorts = 1
+  val numMemPorts = 4
   val sameIDInOrder = true
   val typicalMemLatencyCycles = 32
   val burstBeats = 8  // TODO why cap bursts at 8? AXI can do more
